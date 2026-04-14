3c6f6471ef188965fc10f65b1fb85915132fb7d3
kptudyogamela <kptudyogamela@gmail.com>
2026-04-14 22:12:29 +0530

    fix duplicate import

--- frontend/app/sign-in/[[...sign-in]]/page.jsx
+++ frontend/app/sign-in/[[...sign-in]]/page.jsx
@@ -1,6 +1,15 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
+  const { userId } = auth();
+
+  // 🔥 If already logged in → redirect
+  if (userId) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <SignIn />
